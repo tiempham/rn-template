d1c5ad20e0943ba7ef2349dabc1650884d442365
Tiềm PQ <tiemdesigner@gmail.com>
2023-01-14 12:01:08 +0700

    Test

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,7 @@
 // create function component react native
 import React from 'react'
 import {View, Text} from 'react-native'
-const a = 1
-const b = 1
-const c = 1
+
 const AppSrc = () => {
 	return (
 		<View>
